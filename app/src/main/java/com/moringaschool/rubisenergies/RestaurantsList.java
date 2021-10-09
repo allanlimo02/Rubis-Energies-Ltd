e9f06e0b6bf8bb72ec4f4349409264cc6a4983c9
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-09 12:33:28 +0300

    Added a Logo Created Restaurants Activity

---
+++ app/src/main/java/com/moringaschool/rubisenergies/RestaurantsList.java
@@ -0,0 +1,14 @@
+package com.moringaschool.rubisenergies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants_list);
+    }
+}
