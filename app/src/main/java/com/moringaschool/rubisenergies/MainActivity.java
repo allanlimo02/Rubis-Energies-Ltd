bcdd20b73987a14009669658bf34286c49f14f6f
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-09 12:30:57 +0300

    Added Butterknife and onClick()

--- app/src/main/java/com/moringaschool/rubisenergies/MainActivity.java
+++ app/src/main/java/com/moringaschool/rubisenergies/MainActivity.java
@@ -1,15 +1,31 @@
 package com.moringaschool.rubisenergies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+//    @BindView(R.id.loginButton) Button mLoginButton;
+    private Button mLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+////        ButterKnife.bind(this);
+//        //Creating onclick listener for landing page button
+//        mLoginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Welcome to Rubis Energy!", Toast.LENGTH_LONG).show();
+//
+//            }
+//        });
     }
 
 }
