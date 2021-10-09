de77a2e4a9e4bbd3ca057246e08068f982ce8aa5
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-09 17:37:54 +0300

    Binded sign in views

--- app/src/main/java/com/moringaschool/rubisenergies/SignIn.java
+++ app/src/main/java/com/moringaschool/rubisenergies/SignIn.java
@@ -3,12 +3,23 @@ package com.moringaschool.rubisenergies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignIn extends AppCompatActivity {
+    @BindView(R.id.signin) Button mSignIn;
+    @BindView(R.id.username) EditText mUsername;
+    @BindView(R.id.password) EditText mPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        ButterKnife.bind(this);
+
     }
 }
