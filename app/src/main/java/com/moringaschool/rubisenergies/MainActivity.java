b70b16978a3718c5585840b4bbf7dc1b3b64b100
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-09 12:37:58 +0300

    Added a Logo Created activity to browse Restaurant class

--- app/src/main/java/com/moringaschool/rubisenergies/MainActivity.java
+++ app/src/main/java/com/moringaschool/rubisenergies/MainActivity.java
@@ -5,27 +5,30 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-//    @BindView(R.id.loginButton) Button mLoginButton;
-    private Button mLoginButton;
+    @BindView(R.id.loginButton) Button mLoginButton;
+//    private Button mLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-////        ButterKnife.bind(this);
+        ButterKnife.bind(this);
 //        //Creating onclick listener for landing page button
-//        mLoginButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(MainActivity.this, "Welcome to Rubis Energy!", Toast.LENGTH_LONG).show();
-//
-//            }
-//        });
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Welcome to Rubis Energy!", Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(MainActivity.this, RestaurantsList.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
 }
