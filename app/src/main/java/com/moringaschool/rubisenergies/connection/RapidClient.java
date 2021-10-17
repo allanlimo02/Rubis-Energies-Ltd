c3fc89a312e59bee26f662a553af387d480d0bb4
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-17 21:15:54 +0300

    Updated retrofit method

---
+++ app/src/main/java/com/moringaschool/rubisenergies/connection/RapidClient.java
@@ -0,0 +1,40 @@
+package com.moringaschool.rubisenergies.connection;
+
+import static com.moringaschool.rubisenergies.Contants.RAPIDAPI_KEY;
+import static com.moringaschool.rubisenergies.Contants.baseUrl;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RapidClient {
+    private static Retrofit retrofit = null;
+
+    //retrofit get method
+    public static RapidApi getClient(){
+        if(retrofit==null){
+            OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request request= chain.request().newBuilder()
+                            .addHeader("rapidapi-key",RAPIDAPI_KEY)
+                            .build();
+                    return chain.proceed(request);
+                }
+            }).build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(RapidApi.class);
+    }
+
+
+}
