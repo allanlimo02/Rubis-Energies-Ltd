7e1c605ef52ba08ef0d1a751f5d2a11ccdceb5aa
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-24 19:18:50 +0300

    Update Constants.java

--- app/src/main/java/com/moringaschool/rubisenergies/Constants.java
+++ app/src/main/java/com/moringaschool/rubisenergies/Constants.java
@@ -3,5 +3,6 @@ package com.moringaschool.rubisenergies;
 public class Constants {
     public static final String baseUrl="https://api.yelp.com/v3/";
     public static final String YelpApiKey=BuildConfig.YelpApiKey;
-
+    public static final String LOCATION_PREFERENCE = "location";
+    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
 }
