4eb6a853a18ef703d38fa34d806fb1e46664f735
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-31 15:30:03 +0300

    Updated User registration

---
+++ app/src/main/java/com/moringaschool/rubisenergies/util/OnStartDragListener.java
@@ -0,0 +1,8 @@
+package com.moringaschool.rubisenergies.util;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+
+}
