f2e474c098b2918fd5ba75e4fa5509af3e20ca0f
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-31 15:30:36 +0300

    Updated User registration

--- app/src/main/java/com/moringaschool/rubisenergies/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/rubisenergies/ui/MainActivity.java
@@ -6,12 +6,15 @@ import butterknife.ButterKnife;
 
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.rubisenergies.R;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
@@ -31,11 +34,32 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
     @Override
     public void onClick(View view) {
         if(view==mLoginButton){
-            Intent intent=new Intent(MainActivity.this, SearchFood.class);
+            Intent intent=new Intent(MainActivity.this, SearchEvent.class);
             startActivity(intent);
         }
-
-//                Toast.makeText(MainActivity.this, "Welcome to Rubis Energy! \n Please Log-In to continue", Toast.LENGTH_LONG).show();
-
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //Log out method that helps us to move to  login page
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LogIn.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
