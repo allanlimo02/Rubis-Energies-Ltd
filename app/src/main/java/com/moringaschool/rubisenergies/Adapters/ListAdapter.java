ab4e1c86d8b99c98a7f1337fabae2723a77ebb14
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-31 15:27:54 +0300

    Updated fragment view

--- app/src/main/java/com/moringaschool/rubisenergies/Adapters/ListAdapter.java
+++ app/src/main/java/com/moringaschool/rubisenergies/Adapters/ListAdapter.java
@@ -1,18 +1,23 @@
 package com.moringaschool.rubisenergies.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.moringaschool.rubisenergies.R;
 import com.moringaschool.rubisenergies.models.Event;
+import com.moringaschool.rubisenergies.ui.EventDetail;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -29,26 +34,24 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.eventViewAdapt
     }
     @Override
     public ListAdapter.eventViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_list_item,parent,false);
         eventViewAdapter viewHolder= new eventViewAdapter (view);
-
         return viewHolder;
     }
-
     @Override
     public void onBindViewHolder(@NonNull eventViewAdapter holder, int position) {
         holder.bindEvent(eventList.get(position));
     }
-
     @Override
     public int getItemCount() {
         return eventList.size();
     }
-    public class eventViewAdapter extends RecyclerView.ViewHolder{
+
+// second class
+    public class eventViewAdapter extends RecyclerView.ViewHolder implements View.OnClickListener{
         @BindView(R.id.eventImageView) ImageView eventImageView;
         @BindView(R.id.eventName) TextView eventName;
-        @BindView(R.id.date) TextView date;
+//        @BindView(R.id.date) TextView date;
         @BindView(R.id.category) TextView category;
         @BindView(R.id.url) TextView url;
 
@@ -58,17 +61,25 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.eventViewAdapt
             super(itemView);
             ButterKnife.bind(this,itemView);
             mContext=itemView.getContext();
+
+            itemView.setOnClickListener(this);
         }
         public void bindEvent(Event event) {
             eventName.setText(event.getName());
-            date.setText(event.getTimeStart());
+//            date.setText(event.getTimeStart());
             category.setText(event.getCategory());
             url.setText(event.getTicketsUrl());
             Picasso.get().load(event.getImageUrl()).into(eventImageView);
-
-
         }
-
+        @Override
+        public void onClick(View view) {
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(context, EventDetail.class);
+            intent.putExtra("position", itemPosition);
+            intent.putExtra("eventList", Parcels.wrap(eventList));
+//            Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show();
+            context.startActivity(intent);
+        }
     }
 
 }
