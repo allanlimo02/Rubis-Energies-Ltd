f3f7ce3a4296df5a9f4a37ab6e8c3a7f8ac18271
allanlimo02 <allan.limo@student.moringaschool.com>
2021-10-31 15:28:51 +0300

    Updated User registration

--- app/src/main/java/com/moringaschool/rubisenergies/ui/SearchEvent.java
+++ app/src/main/java/com/moringaschool/rubisenergies/ui/SearchEvent.java
@@ -6,12 +6,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -21,15 +18,13 @@ import com.moringaschool.rubisenergies.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SearchFood extends AppCompatActivity implements View.OnClickListener{
+public class SearchEvent extends AppCompatActivity implements View.OnClickListener{
 
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
     private String mRecentAddress;
 
     private DatabaseReference mSearchedLocation;
-
-
 
     @BindView(R.id.food_search) EditText food_search;
     @BindView(R.id.submit) Button submit;
@@ -52,38 +47,36 @@ public class SearchFood extends AppCompatActivity implements View.OnClickListene
         submit.setOnClickListener(this);
         sharedPreference.setOnClickListener(this);
 
-//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-//        mEditor=mSharedPreferences.edit();
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor=mSharedPreferences.edit();
+
+//        if(mSharedPreferences.equals("")){
+//
+//        }
 //        mRecentAddress = mSharedPreferences.getString(Constants.LOCATION_PREFERENCE, null);
 //        Log.d("Here is message ","Here is the address "+mRecentAddress);
     }
     @Override
     public void onClick(View view) {
         if(view==submit){
-            if(submit.length()==0){
-                Intent intent=new Intent(SearchFood.this, AllEvents.class);
-                intent.putExtra("eventSearch",mRecentAddress);
-                startActivity(intent);
-            }else {
                 String eventSearch=food_search.getText().toString();
-//                addToSharedPreferences(eventSearch);
+                addToSharedPreferences(eventSearch);
                 saveLocationToFirebase(eventSearch);
-                Intent intent=new Intent(SearchFood.this, AllEvents.class);
+                Intent intent=new Intent(SearchEvent.this, AllEvents.class);
                 intent.putExtra("eventSearch",eventSearch);
                 startActivity(intent);
-            }
         }
         if(view==sharedPreference){
-            Intent intent=new Intent(SearchFood.this, AllEvents.class);
+            Intent intent=new Intent(SearchEvent.this, AllEvents.class);
             intent.putExtra("eventSearch",mRecentAddress);
             startActivity(intent);
 
         }
     }
     //save into shared preference
-//    private void addToSharedPreferences(String eventSearch) {
-//        mEditor.putString(Constants.LOCATION_PREFERENCE, eventSearch).apply();
-//    }
+    private void addToSharedPreferences(String eventSearch) {
+        mEditor.putString(Constants.LOCATION_PREFERENCE, eventSearch).apply();
+    }
     //Save into database
 
     public void saveLocationToFirebase(String location) {
